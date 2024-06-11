47a7c1eb4c7d87aad1d86eb7da17e9f256743637
Falguni Saha <user.devtestacc@gmail.com>
2024-06-11 19:48:45 -0400

    files are updated

--- app/src/main/java/com/takehome/falgunisahatakehome/view/screens/FragmentUserInformationScreen.kt
+++ app/src/main/java/com/takehome/falgunisahatakehome/view/screens/FragmentUserInformationScreen.kt
@@ -1,7 +1,6 @@
 package com.takehome.falgunisahatakehome.view.screens
 
 import android.app.AlertDialog
-import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,7 +9,6 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -24,7 +22,7 @@ import com.takehome.falgunisahatakehome.model.UserRepoResponse
 import com.takehome.falgunisahatakehome.view.MainActivity
 import com.takehome.falgunisahatakehome.viewmodel.MainViewModel
 
-class FragmentUserInformationScreen: Fragment(),ListAdapter.ClickListener {
+class FragmentUserInformationScreen : Fragment(), ListAdapter.ClickListener {
     private lateinit var mainViewModel: MainViewModel
     private lateinit var btnSend: Button
     private lateinit var recyclerview: RecyclerView
@@ -33,65 +31,80 @@ class FragmentUserInformationScreen: Fragment(),ListAdapter.ClickListener {
     private lateinit var searchText: EditText
     private var totalNumberOfForks = 0
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val userInformation =LayoutInflater.from(container!!.context).inflate(R.layout.layout_fragment_user_information_screen,container,false)
-        activity?.onBackPressedDispatcher?.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-               requireActivity().finish()
-            }
-        })
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val userInformation = LayoutInflater.from(container!!.context)
+            .inflate(R.layout.layout_fragment_user_information_screen, container, false)
+        activity?.onBackPressedDispatcher?.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    requireActivity().finish()
+                }
+            })
         return userInformation
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        mainViewModel  = ViewModelProvider(requireActivity())[MainViewModel::class.java]
+        mainViewModel = ViewModelProvider(requireActivity())[MainViewModel::class.java]
         btnSend = view.findViewById(R.id.searchButton)
         setUpViews(view)
         subscribe()
         btnSend.setOnClickListener {
-            val searchItem = searchText.text?:null
+            val searchItem = searchText.text ?: null
             mainViewModel.getUserData(searchItem.toString())
             mainViewModel.getRepoList(searchItem.toString())
         }
     }
 
-    private fun setUpViews(view: View){
+    /**
+     * @method to set the views
+     */
+    private fun setUpViews(view: View) {
         userName = view.findViewById(R.id.userNameText)
         userImage = view.findViewById(R.id.avatarImg)
         searchText = view.findViewById(R.id.searchText)
         recyclerview = view.findViewById(R.id.repoRecyclerview)
         recyclerview.layoutManager = LinearLayoutManager(activity)
     }
+
+    /**
+     * @method to register all the subscriber
+     */
     private fun subscribe() {
         val dialog = AlertDialog.Builder(requireActivity()).setMessage(R.string.loading).create()
         activity?.let {
-            mainViewModel.isLoading.observe(it) { isLoading ->
-                if(isLoading) dialog.show() else dialog.cancel()
+            mainViewModel.getRepositoryInstance().isLoading.observe(it) { isLoading ->
+                if (isLoading) dialog.show() else dialog.cancel()
             }
         }
 
         activity?.let {
             val errorDialog =
                 AlertDialog.Builder(requireActivity()).setMessage(R.string.loading).create()
-            mainViewModel.isError.observe(it) { isError ->
+            mainViewModel.getRepositoryInstance().isError.observe(it) { isError ->
                 if (isError) errorDialog.show() else errorDialog.cancel()
             }
         }
 
         activity?.let {
-            mainViewModel.userData.observe(it) { data ->
+            mainViewModel.getRepositoryInstance().userData.observe(it) { data ->
                 userName.text = data.name
                 Glide.with(this).load(data.avatarUrl).into(userImage)
             }
         }
         activity?.let {
-            mainViewModel.userRepo.observe(it) { data ->
+            mainViewModel.getRepositoryInstance().userRepo.observe(it) { data ->
                 val adapter = ListAdapter(data, this)
                 // Setting the Adapter with the recyclerview
                 recyclerview.adapter = adapter
                 recyclerview.isNestedScrollingEnabled = false
                 totalNumberOfForks = 0
-                    for(item in data) totalNumberOfForks += item.forks
+                for (item in data) totalNumberOfForks += item.forks
             }
         }
     }
@@ -101,5 +114,4 @@ class FragmentUserInformationScreen: Fragment(),ListAdapter.ClickListener {
         (activity as MainActivity).replaceWithDetailScreen()
 
     }
-
 }
--- app/src/main/java/com/takehome/falgunisahatakehome/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/takehome/falgunisahatakehome/viewmodel/MainViewModel.kt
@@ -4,34 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.takehome.falgunisahatakehome.model.DetailScreenModel
-import com.takehome.falgunisahatakehome.service.ApiConfigService
-import com.takehome.falgunisahatakehome.model.UserInfoResponse
-import com.takehome.falgunisahatakehome.model.UserRepoResponse
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.takehome.falgunisahatakehome.repository.UserRepository
 
-
-class MainViewModel(): ViewModel() {
-
-    private val _UserData = MutableLiveData<UserInfoResponse>()
-    val userData: LiveData<UserInfoResponse> get() = _UserData
-
-    private val _UserRepo = MutableLiveData<List<UserRepoResponse>>()
-    val userRepo: LiveData<List<UserRepoResponse>> get() = _UserRepo
-
-    private val _isLoading = MutableLiveData<Boolean>()
-    val isLoading: LiveData<Boolean> get() = _isLoading
-
-    private val _isError = MutableLiveData<Boolean>()
-    val isError: LiveData<Boolean> get() = _isError
-
-    var errorMessage: String = ""
-        private set
-
+class MainViewModel() : ViewModel() {
+    private val userRepository = UserRepository()
     private val _userDetail = MutableLiveData<DetailScreenModel>()
     val userDetail: LiveData<DetailScreenModel> get() = _userDetail
 
@@ -40,92 +16,25 @@ class MainViewModel(): ViewModel() {
      * @param userId is entered by the user
      */
     fun getUserData(userId: String) {
-
-        _isLoading.value = true
-        _isError.value = false
-
-        val client = ApiConfigService.getApiService().getUserInfo(userId)
-
-        // Send API request using Retrofit
-       CoroutineScope(Dispatchers.IO).launch {
-        client.enqueue(object : Callback<UserInfoResponse> {
-
-            override fun onResponse(
-                call: Call<UserInfoResponse>,
-                response: Response<UserInfoResponse>
-            ) {
-                val responseBody = response.body()
-                if (!response.isSuccessful || responseBody == null) {
-                        onError("Data Processing Error")
-                        return
-                    }
-                    _isLoading.value = false
-                    _UserData.postValue(responseBody)
-            }
-
-            override fun onFailure(call: Call<UserInfoResponse>, t: Throwable) {
-                onError(t.message)
-                t.printStackTrace()
-            }
-
-        })}
+        userRepository.fetchUserInformation(userId)
     }
 
     /**
      * @method to get the repo details for a particular repo
      * @param userId is entered by the user
      */
-    fun getRepoList(userId: String){
-        _isLoading.value = true
-        _isError.value = false
-
-        val client = ApiConfigService.getApiService().getUserRepo(userId)
-        CoroutineScope(Dispatchers.IO).launch {
-        // Send API request using Retrofit
-        client.enqueue(object : Callback<List<UserRepoResponse>> {
-
-            override fun onResponse(
-                call: Call<List<UserRepoResponse>>,
-                response: Response<List<UserRepoResponse>>
-            ) {
-                val responseBody = response.body()
-
-                if (!response.isSuccessful || responseBody == null) {
-                    onError("Data Processing Error")
-                    return
-                }
-                _isLoading.value = false
-                _UserRepo.postValue(responseBody)
-            }
-
-            override fun onFailure(call: Call<List<UserRepoResponse>>, t: Throwable) {
-                onError(t.message)
-                t.printStackTrace()
-            }
-        })}
-    }
-
-    /**
-     * @method to set the error message
-     * @param inputMessage  is the error message
-     */
-    private fun onError(inputMessage: String?) {
-
-        val message = if (inputMessage.isNullOrBlank() or inputMessage.isNullOrEmpty()) "Unknown Error"
-        else inputMessage
-
-        errorMessage = StringBuilder("ERROR: ")
-            .append("$message some data may not displayed properly").toString()
-
-        _isError.value = true
-        _isLoading.value = false
+    fun getRepoList(userId: String) {
+        userRepository.fetchUserRepo(userId)
     }
 
     /**
      * @method to send the click events to open the details screen
      * @param detailScreen contains the data to be shown on the details screen
      */
-    fun sendClickEvents(detailScreen: DetailScreenModel){
-         _userDetail.postValue(detailScreen)
+    fun sendClickEvents(detailScreen: DetailScreenModel) {
+        _userDetail.postValue(detailScreen)
     }
+
+    fun getRepositoryInstance() : UserRepository = userRepository
 }
+
